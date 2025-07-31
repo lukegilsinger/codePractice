feb6da8de60dfa9cee5b333b7c0015e43fb9f3f0
lukegilsingerbayer <luke.gilsinger.ext@bayer.com>
2025-07-31 11:39:18 -0500

    environments

--- golang/cmd/my-go-backend/go.mod
+++ golang/cmd/my-go-backend/go.mod
@@ -4,6 +4,7 @@ go 1.24.1
 
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.30 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 )
--- golang/cmd/my-go-backend/main.go
+++ golang/cmd/my-go-backend/main.go
@@ -9,7 +9,9 @@ import (
 	"os"
 	"strconv"
 
+	// "github.com/docker/docker/daemon/logger"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 	// "google.golang.org/protobuf/proto"
 	// pb "my-go-backend/pb"
@@ -28,12 +30,74 @@ type Task struct {
 	DueDate     string `json:"due_date"`
 }
 
+type Config struct {
+	Env string `env:"ENV,required"`
+}
+
 var db *sql.DB
 
-func initDB() {
-	fmt.Println("initializing sqlite db")
+func main() {
+	// Load environment variables from .env file
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+
+	// Determine the environment and load the corresponding .env file
+	env := os.Getenv("GO_ENV") // Set this environment variable to "development", "staging", or "production"
+	if env == "" {
+		env = "dev" // Default to development if not set
+	}
+
+	envFile := "environments/" + env + ".env"
+	err := godotenv.Load(envFile)
+	if err != nil {
+		fmt.Printf("No %s file found, using default environment variables", envFile)
+	}
+
+	fmt.Println("ENV: " + os.Getenv("ENV"))
+	// cfg := Config{} // 👈 new instance of `Config`
+
+	// err = env.Parse(&cfg) // 👈 Parse environment variables into `Config`
+	// if err != nil {
+	// 	log.Fatalf("unable to parse ennvironment variables: %e", err)
+	// }
+
+	logger.Info("Info message", "key2", "value2")
+	initDB(os.Getenv("DATABASE_FILE"))
+	defer db.Close()
+
+	r := mux.NewRouter()
+
+	// http.HandleFunc("/add-item", addItemHandler)
+	// http.HandleFunc("/get-items", getItemsHandler)
+
+	// http.HandleFunc("/add-task", createTaskHandler)
+	http.HandleFunc("/get-tasks", getTasksHandler)
+	// http.HandleFunc("/update-task", updateTaskHandler)
+	// http.HandleFunc("/delete-task", deleteTaskHandler)
+
+	r.HandleFunc("/tasks", getTasksHandler).Methods("GET")                 // GET /tasks
+	r.HandleFunc("/task", createTaskHandler).Methods("POST")               // POST /tasks
+	r.HandleFunc("/task/{id:[0-9]+}", getTaskHandler).Methods("GET")       // GET /tasks/{id}
+	r.HandleFunc("/task/{id:[0-9]+}", updateTaskHandler).Methods("PUT")    // PUT /tasks/{id}
+	r.HandleFunc("/task/{id:[0-9]+}", deleteTaskHandler).Methods("DELETE") // DELETE /tasks/
+
+	// http.Handle("/", http.FileServer(http.Dir("./web"))
+
+	// Serve static files from the "web" directory
+	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./web/"))))
+	http.Handle("/", r)
+
+	port := os.Getenv("PORT")
+	fmt.Printf("Server is running on port %v\n", port)
+	if err := http.ListenAndServe(":"+port, r); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func initDB(databaseFile string) {
+	fmt.Println("initializing sqlite db from " + databaseFile)
 	var err error
-	db, err = sql.Open("sqlite3", "./database.db")
+
+	db, err = sql.Open("sqlite3", "./"+databaseFile)
 	if err != nil {
 		panic(err)
 	}
@@ -253,39 +317,4 @@ func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("deleted task %v\n", id)
 
 	w.WriteHeader(http.StatusNoContent)
-}
-
-func main() {
-
-	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
-	logger.Info("Info message", "key2", "value2")
-	initDB()
-	defer db.Close()
-
-	r := mux.NewRouter()
-
-	// http.HandleFunc("/add-item", addItemHandler)
-	// http.HandleFunc("/get-items", getItemsHandler)
-
-	// http.HandleFunc("/add-task", createTaskHandler)
-	http.HandleFunc("/get-tasks", getTasksHandler)
-	// http.HandleFunc("/update-task", updateTaskHandler)
-	// http.HandleFunc("/delete-task", deleteTaskHandler)
-
-	r.HandleFunc("/tasks", getTasksHandler).Methods("GET")                 // GET /tasks
-	r.HandleFunc("/task", createTaskHandler).Methods("POST")               // POST /tasks
-	r.HandleFunc("/task/{id:[0-9]+}", getTaskHandler).Methods("GET")       // GET /tasks/{id}
-	r.HandleFunc("/task/{id:[0-9]+}", updateTaskHandler).Methods("PUT")    // PUT /tasks/{id}
-	r.HandleFunc("/task/{id:[0-9]+}", deleteTaskHandler).Methods("DELETE") // DELETE /tasks/
-
-	// http.Handle("/", http.FileServer(http.Dir("./web"))
-
-	// Serve static files from the "web" directory
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./web/"))))
-	http.Handle("/", r)
-
-	fmt.Println("Server is running on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
-	}
 }
